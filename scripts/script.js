d0be52c35ccd146b34c6dbef2870f644826e20c9
muhammet075 <34505894+muhammet075@users.noreply.github.com>
2020-09-24 18:16:55 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
 // JavaScript Document
+
+var hamburgermenu = document.querySelector(".hamburger-menu");
+var checkbox = document.querySelector("#toggle");
+var sluiten = document.querySelector(".menu-sluiten");
+
+checkbox.addEventListener("click", openHamburgermenu);
+
+
+function openHamburgermenu() {
+
+	if (checkbox.checked == true) {
+
+		//de smokeramen worden getoond
+		console.log("re");
+		hamburgermenu.classList.remove("displaynone");
+
+
+	} else {
+
+		hamburgermenu.classList.add("displaynone");
+
+	};
+
+};
+
+
+sluiten.addEventListener("click", sluitHamburgermenu);
+
+function sluitHamburgermenu() {
+			hamburgermenu.classList.add("displaynone");
+}
